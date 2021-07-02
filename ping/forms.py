d6c283ee110f60f97b176f63628d87b4c0f0ae75
aldotele <aldo.telese@hotmail.it>
2021-07-02 10:30:01 +0200

    regexp max length extended

--- ping/forms.py
+++ ping/forms.py
@@ -5,7 +5,7 @@ from ping.models import Url
 class UrlForm(forms.Form):
     url = forms.URLField(widget=forms.URLInput(attrs={'placeholder': 'Enter Url'}), label=False, max_length=1024)
     regexp = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter regexp (optional)'}),
-                             label=False, max_length=100, required=False)
+                             label=False, max_length=1024, required=False)
 
     class Meta:
         model = Url
