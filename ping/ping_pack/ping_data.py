5169ae7fd323f3e854b3e4ec4f47dc91ffbdad94
aldotele <aldo.telese@hotmail.it>
2021-07-02 17:42:11 +0200

    regex match and boolean returns

--- ping/ping_pack/ping_data.py
+++ ping/ping_pack/ping_data.py
@@ -17,9 +17,9 @@ class Ping:
     @staticmethod
     def connect_attempt(url):
         try:
-            r = requests.get(url, timeout=6)
-            r.raise_for_status()
-            return r
+            response = requests.get(url, timeout=6)
+            #r.raise_for_status()  # it won't allow to save other status codes
+            return response
         except:
             raise ConnectionError("unable to connect")
 
@@ -27,16 +27,16 @@ class Ping:
     def is_regexp_matching(regular_expression, content):
         #print(regular_expression)
         try:
-            out = re.search(regular_expression, content)  # check this !!! previously: re.search
+            out = re.match(regular_expression, content)  # check this !!! previously: re.search
             #print(out)
         except:
             return "", ""
         if out and regular_expression != '':
-            return "Yes", out
+            return True, out.group()  # the second return is the matching string
         elif regular_expression == '':
-            return "", ""
+            return None, None
         else:
-            return "No", ""
+            return False, ""
 
     @staticmethod
     def save_ping_data(url, status_code, response_time, regexp, is_regexp_matching, matching_details):
@@ -44,18 +44,6 @@ class Ping:
                                       regexp=regexp, regexp_match=is_regexp_matching, match_details=matching_details)
         url_ping.save()
 
-
-"""
-    def get_status_code(self):
-        return self.r.status_code
-
-    def get_response_time(self):
-        resp_time = str(round(self.r.elapsed.total_seconds(), 2))
-        return resp_time
-
-    def get_response_content(self):
-        return self.r.text
-"""
 
 if __name__ == '__main__':
     url = 'http://google.com/'
