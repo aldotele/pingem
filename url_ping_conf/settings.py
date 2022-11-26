2037d5e9b81e4674f3458770ff44be309598e236
aldotele <aldo.telese@hotmail.it>
2022-11-26 15:53:11 +0100

    rm import django heroku

--- url_ping_conf/settings.py
+++ url_ping_conf/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from environs import Env
-import django_heroku
 
 
 env = Env()
@@ -125,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
