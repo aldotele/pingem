d110f353ff7493c8f49488714c30d08206dfd9f2
aldotele <aldo.telese@hotmail.it>
2022-11-26 15:56:52 +0100

    adds secret key

--- url_ping_conf/settings.py
+++ url_ping_conf/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = "*5$t_=c5ws1!!4j*xf6zdk&7c-eq5=1m3a^%z@+#(yq^_4#xw_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
